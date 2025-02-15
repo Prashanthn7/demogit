e39bfbeeef7b7131916e2f45e646a9ea50c0d0ef
Prashanthn7 <pn342604gmail.com>
2025-02-15 16:09:09 +0530

    Adding the entity class

---
+++ src/main/java/com/example/git/demo/model/User.java
@@ -0,0 +1,41 @@
+package com.example.git.demo.model;
+
+@entity
+public class User {
+    private String username;
+    private int userId;
+    private int userAge;
+    private String userEmail;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getUserAge() {
+        return userAge;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public void setUserAge(int userAge) {
+        this.userAge = userAge;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+}
